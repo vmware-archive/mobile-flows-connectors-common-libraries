c1d24a7e9153adcb9d1685f6ddf59f2fd728805f
Shree Harsha S <harshas@vmware.com>
2020-02-27 13:20:57 +0530

    APF-2913 Initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+/*
+* Copyright © 2020 VMware, Inc. All Rights Reserved.
+* SPDX-License-Identifier: BSD-2-Clause
+*/
+
+'use strict'
---
+++ test/index-test.js
@@ -0,0 +1,6 @@
+'use strict'
+
+
+describe('Mobile Flows connectors common tests', () => {
+
+})

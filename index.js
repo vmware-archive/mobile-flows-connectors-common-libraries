f6ea3d392ec7faabc5f71998ed19afb25888d471
Shree Harsha S <harshas@vmware.com>
2020-02-27 19:19:57 +0530

    APF-2913 Export common functions.

--- index.js
+++ index.js
@@ -4,3 +4,15 @@
 */
 
 'use strict'
+
+const { getConnectorBaseUrl, handleXRequestId } = require('./lib/utility')
+const connectorAuth = require('./lib/connector-auth')
+const { log, logReq } = require('./lib/log')
+
+module.exports = Object.freeze({
+    getConnectorBaseUrl: getConnectorBaseUrl,
+    handleXRequestId: handleXRequestId,
+    validateConnectorAuth: connectorAuth.validate,
+    log: log,
+    logReq: logReq
+})

a5d8ec8411558a84d913d3fb936c616901595e28
Shree Harsha S <harshas@vmware.com>
2020-02-28 16:21:47 +0530

    APF-2913 Copy files from HelpNow chatbot connector.

--- index.js
+++ index.js
@@ -7,12 +7,16 @@
 
 const { getConnectorBaseUrl, handleXRequestId } = require('./lib/utility')
 const auth = require('./lib/auth')
+const mfRouting = require('./lib/mf-routing')
+const mockMfServer = require('./lib/mock-mf-server')
 const { log, logReq } = require('./lib/log')
 
 module.exports = Object.freeze({
   getConnectorBaseUrl,
   handleXRequestId,
   validateAuth: auth.validate,
+  mfRouting,
+  mockMfServer,
   log,
   logReq
 })
